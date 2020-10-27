58d040d591417ee26b8312c5757953391a141cbf
JoelKanyi <joelkanyi98@gmail.com>
2020-10-27 19:49:17 +0300

    worked on both the side and bottom navs

--- app/src/main/java/com/ifixhubke/trendyhairstyles/MainActivity.java
+++ app/src/main/java/com/ifixhubke/trendyhairstyles/MainActivity.java
@@ -6,17 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout mDrawer;
     Toolbar mToolbar;
     NavigationView mNavigationView;
-
     ActionBarDrawerToggle mActionBarDrawerToggle;
 
     @Override
@@ -24,21 +26,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        //setSupportActionBar(mToolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       // mToolbar = findViewById(R.id.my_toolbar);
+        mDrawer = findViewById(R.id.drawerLayout);
+        mNavigationView = findViewById(R.id.nav_view);
+       // mToolbar.setTitle("Home");
+       // setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Home");
 
-        mDrawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawer,R.string.open,R.string.close);
+        mDrawer.addDrawerListener(mActionBarDrawerToggle);
+        mActionBarDrawerToggle.syncState();
+
+        mNavigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId()){
-            case R.id.bottom_nav_home:
-                mDrawer.openDrawer(GravityCompat.START);
-                return true;
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void displaySelectedNavMenu(int itemID){
+        switch (itemID){
+            case R.id.nav_switch:{
+                Toast.makeText(this, "Dark mode switch", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.nav_about:{
+                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.nav_report:{
+                Toast.makeText(this, "Report", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.nav_settings:{
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.nav_logout:{
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+            }
+
+        }
+        mDrawer.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        displaySelectedNavMenu(menuItem.getItemId());
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawer.isDrawerOpen(GravityCompat.START)){
+            mDrawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
