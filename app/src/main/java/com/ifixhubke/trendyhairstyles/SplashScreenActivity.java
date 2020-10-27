3f599613db39d6c75e9e2c5964dbbbaeb354687e
JoelKanyi <joelkanyi98@gmail.com>
2020-10-27 13:06:38 +0300

    worked on the splash screen

--- app/src/main/java/com/ifixhubke/trendyhairstyles/SplashScreenActivity.java
+++ app/src/main/java/com/ifixhubke/trendyhairstyles/SplashScreenActivity.java
@@ -2,7 +2,10 @@ package com.ifixhubke.trendyhairstyles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            }
+        },4000);
     }
 }
