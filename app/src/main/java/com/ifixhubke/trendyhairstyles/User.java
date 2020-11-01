5b1115f3398224d5d0be90f0acddeaaee93d227f
JoelKanyi <joelkanyi98@gmail.com>
2020-11-01 18:09:06 +0300

    fixed app crash: No properties to serialize found on class User

--- app/src/main/java/com/ifixhubke/trendyhairstyles/User.java
+++ app/src/main/java/com/ifixhubke/trendyhairstyles/User.java
@@ -3,6 +3,31 @@ package com.ifixhubke.trendyhairstyles;
 public class User {
 
     String full_names;
+
+    public String getFull_names() {
+        return full_names;
+    }
+
+    public void setFull_names(String full_names) {
+        this.full_names = full_names;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public String getAbout_you() {
+        return about_you;
+    }
+
+    public void setAbout_you(String about_you) {
+        this.about_you = about_you;
+    }
+
     String email_address;
     String about_you;
 
